d673a31bfb7897ce10c5ee94eba1c58607151867
lucasmoraessouza <lucasmoraes_souza@outlook.com>
2024-12-30 21:47:39 -0300

    :bug: fix: path background image to deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         primary: "#1A1A1A",
-        menu: "#0F0F0F"
+        menu: "#0F0F0F",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
@@ -14,8 +14,8 @@ export default {
         serrat: ["Montserrat", "sans-serif"],
       },
       backgroundImage: {
-        "grid-mobile": "url('./src/assets/images/rectangle-grid-mobile.png')",
-        "grid-desk": "url('./src/assets/images/rectangle-grid.png')",
+        "grid-mobile": "url('/src/assets/images/rectangle-grid-mobile.png')",
+        "grid-desk": "url('/src/assets/images/rectangle-grid.png')",
         "text-gradient": "linear-gradient(to right, #4FC3F7, #FFFFFF)",
       },
     },

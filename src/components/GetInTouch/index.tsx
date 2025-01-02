44f488a52552f97d2425e87c21329a7c194b40b1
lucasmoraessouza <lucasmoraes_souza@outlook.com>
2025-01-02 20:04:40 -0300

    :sparkles: feat: add validation and loading icon

--- src/components/GetInTouch/index.tsx
+++ src/components/GetInTouch/index.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import emailjs from "emailjs-com";
 import { enqueueSnackbar } from "notistack";
 import SendIcon from "../../assets/icons/send-icon";
+import ReactLoading from "react-loading";
 
 export function GetInTouch() {
+  const [loading, setLoading] = useState<boolean>(false);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,7 +25,7 @@ export function GetInTouch() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
+    setLoading(true);
     const serviceId = import.meta.env.VITE_SERVICE_ID!;
     const templateId = import.meta.env.VITE_TEMPLATE_ID!;
     const userId = import.meta.env.VITE_USER_ID!;
@@ -33,28 +35,34 @@ export function GetInTouch() {
       alert(
         "Configuração do EmailJS ausente. Verifique as variáveis de ambiente."
       );
+      setLoading(false);
       return;
     }
 
-    emailjs.send(serviceId, templateId, formData, userId).then(
-      () => {
-        enqueueSnackbar("Mensagem enviada com sucesso!", {
-          variant: "success",
-        });
-        setFormData({
-          name: "",
-          email: "",
-          subject: "",
-          message: "",
-        });
-      },
-      (err) => {
-        console.log(err);
-        enqueueSnackbar("Falha ao enviar a mensagem. Tente novamente.", {
-          variant: "error",
-        });
-      }
-    );
+    emailjs
+      .send(serviceId, templateId, formData, userId)
+      .then(
+        () => {
+          enqueueSnackbar("Mensagem enviada com sucesso!", {
+            variant: "success",
+          });
+          setFormData({
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          });
+        },
+        (err) => {
+          console.log(err);
+          enqueueSnackbar("Falha ao enviar a mensagem. Tente novamente.", {
+            variant: "error",
+          });
+        }
+      )
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -70,46 +78,60 @@ export function GetInTouch() {
         <input
           id="name"
           name="name"
-          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent focus-visible:outline-none"
+          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent"
           value={formData.name}
           onChange={handleChange}
-          // required
+          required
         />
         <label htmlFor="email">Email</label>
         <input
           id="email"
           name="email"
-          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent focus-visible:outline-none"
+          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent"
           value={formData.email}
           onChange={handleChange}
-          // required
+          required
         />
         <label htmlFor="subject">Assunto</label>
         <input
           id="subject"
           name="subject"
-          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent focus-visible:outline-none"
+          className="border-white border-[2px] rounded-[10px] h-[55px] px-4 bg-transparent"
           value={formData.subject}
           onChange={handleChange}
-          // required
+          required
         />
         <label htmlFor="message">Mensagem</label>
         <textarea
           id="message"
           name="message"
-          className="border-white border-[2px] rounded-[10px] p-4 bg-transparent focus-visible:outline-none"
+          className="border-white border-[2px] rounded-[10px] p-4 bg-transparent"
           rows={6}
           value={formData.message}
           onChange={handleChange}
-          // required
+          required
         ></textarea>
         <div className="mt-8 w-full">
           <button
             type="submit"
-            className="bg-white rounded-[10px] text-black h-[55px] w-full flex justify-center items-center gap-2"
+            className={`bg-white rounded-[10px] text-black h-[55px] flex justify-center items-center gap-2 w-full transition-colors duration-300 ${
+              loading ? "bg-gray-400 cursor-not-allowed" : "hover:bg-gray-200"
+            }`}
+            disabled={loading}
           >
-            <SendIcon />
-            Enviar mensagem
+            {loading ? (
+              <ReactLoading
+                type="bubbles"
+                color="#1A1A1A"
+                height={40}
+                width={40}
+              />
+            ) : (
+              <>
+                <SendIcon />
+                <span>Enviar mensagem</span>
+              </>
+            )}
           </button>
         </div>
       </form>
